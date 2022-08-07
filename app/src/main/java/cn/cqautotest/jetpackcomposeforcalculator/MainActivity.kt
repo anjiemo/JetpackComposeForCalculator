d8a3d7adc72c72b4b62c3bbdd8adfde6bef5fc43
anjiemo <2695734816@qq.com>
2022-08-07 22:07:03 +0800

    新增归零功能，解决多次点击等号时会持续计算的问题。

--- app/src/main/java/cn/cqautotest/jetpackcomposeforcalculator/MainActivity.kt
+++ app/src/main/java/cn/cqautotest/jetpackcomposeforcalculator/MainActivity.kt
@@ -4,94 +4,119 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import cn.cqautotest.jetpackcomposeforcalculator.ui.theme.JetpackComposeForCalculatorTheme
+import androidx.compose.ui.unit.sp
+import cn.cqautotest.jetpackcomposeforcalculator.ui.theme.Background
+import cn.cqautotest.jetpackcomposeforcalculator.ui.theme.DarkGray
+import cn.cqautotest.jetpackcomposeforcalculator.ui.theme.LightGray
+import cn.cqautotest.jetpackcomposeforcalculator.ui.theme.Orange
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeForCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    HomeScreen()
-                }
-            }
+            Calculator()
         }
     }
 }
 
-private const val TAG = "MainActivity"
+// 计算器按钮
+val buttons = arrayOf(
+    arrayOf("AC" to LightGray, "+/-" to LightGray, "%" to LightGray, "÷" to Orange),
+    arrayOf("7" to DarkGray, "8" to DarkGray, "9" to DarkGray, "×" to Orange),
+    arrayOf("4" to DarkGray, "5" to DarkGray, "6" to DarkGray, "-" to Orange),
+    arrayOf("1" to DarkGray, "2" to DarkGray, "3" to DarkGray, "+" to Orange),
+    arrayOf("0" to DarkGray, "." to DarkGray, "=" to Orange),
+)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeForCalculatorTheme {
-        HomeScreen()
+// 计算器状态
+data class CalculatorState(val number1: Int = 0, val number2: Int = 0, val opt: String? = null)
+
+// 计算逻辑
+fun calculate(currState: CalculatorState, input: String): CalculatorState {
+    return when (input) {
+        in "0".."9" -> currState.copy(number2 = input.toInt(), number1 = currState.number2)
+        in arrayOf("+", "-", "×", "÷") -> currState.copy(opt = input)
+        "=" -> {
+            when (currState.opt) {
+                "+" -> currState.copy(number2 = currState.number1 + currState.number2, opt = null)
+                "-" -> currState.copy(number2 = currState.number1 - currState.number2, opt = null)
+                "×" -> currState.copy(number2 = currState.number1 * currState.number2, opt = null)
+                "÷" -> currState.copy(number2 = currState.number1 / currState.number2, opt = null)
+                else -> currState
+            }
+        }
+        "AC" -> currState.copy(number1 = 0, number2 = 0, opt = null)
+        else -> currState
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-private fun HomeScreen() {
-    var text by remember { mutableStateOf("0") }
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        // input box
-        Row(
+fun Calculator() {
+
+    var state by remember {
+        mutableStateOf(CalculatorState())
+    }
+
+    Column(
+        modifier = Modifier
+            .background(Background)
+            .padding(horizontal = 10.dp)
+    ) {
+        Box(
             modifier = Modifier
-                .padding(start = 10.dp, top = 10.dp, end = 10.dp, bottom = 0.dp)
-                .align(alignment = Alignment.End)
+                .fillMaxHeight(0.3f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomEnd
         ) {
-            InputComponent(text)
+            Text(text = state.number2.toString(), fontSize = 100.sp, color = Color.White)
         }
-        // number keyboard panel
-        Box(modifier = Modifier.padding(10.dp)) {
-            NumericKeypadPanel { key ->
-                text = key
+        Column(modifier = Modifier.fillMaxSize()) {
+            buttons.forEach {
+                Row(
+                    modifier = Modifier
+                        .weight(1f),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    it.forEach {
+                        CalculatorButton(
+                            modifier = Modifier
+                                .weight(if (it.first == "0") 2f else 1f)
+                                // 纵横比例
+                                .aspectRatio(if (it.first == "0") 2f else 1f)
+                                .background(it.second),
+                            symbol = it.first
+                        ) {
+                            state = calculate(state, it.first)
+                        }
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun NumericKeypadPanel(onKeyClick: (text: String) -> Unit) {
-    val keyArr = arrayOf(
-        arrayOf("AC", "DEL", "%", "÷"),
-        arrayOf("7", "8", "9", "×"),
-        arrayOf("4", "5", "6", "-"),
-        arrayOf("1", "2", "3", "+"),
-        arrayOf("0", ".", "=", "Exit"),
-    )
-    NumericKeypadGridPanel(keyArr = keyArr, onKeyClick = onKeyClick)
-}
-
-@Composable
-fun InputComponent(text: String) {
-    Text(text = text, modifier = Modifier.background(color = Color.Transparent))
-}
-
-@Composable
-fun NumericKeypadGridPanel(keyArr: Array<Array<String>>, onKeyClick: (text: String) -> Unit) {
-    Column {
-        keyArr.forEach {
-            Row {
-                val lastIndex = it.lastIndex
-                it.forEachIndexed { index, key ->
-                    Button(
-                        onClick = { onKeyClick.invoke(key) },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
-                    ) {
-                        Text(text = key)
-                    }
-                    takeUnless { index == lastIndex }?.let { Spacer(modifier = Modifier.width(14.dp)) }
-                }
-            }
-        }
+fun CalculatorButton(modifier: Modifier, symbol: String, onClick: () -> Unit = {}) {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .then(modifier)
+            .clickable { onClick.invoke() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = symbol, color = Color.White)
     }
 }
